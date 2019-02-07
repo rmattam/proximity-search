8551cb879427eb27df59a32dbd3e3676e3745b99
rmattam <rahulroymattam@gmail.com>
2019-02-06 20:32:10 -0700

    positional index + proximity searching

---
+++ src/main/scala/search/PositionalIndex.scala
@@ -0,0 +1,88 @@
+package search
+
+import scala.collection.mutable.{ArrayBuffer, Map}
+import scala.util.control.Breaks._
+
+class PositionalIndex {
+
+  /*
+  The value of this map is a tuple posting list with the first element storing the document id,
+  and the second element stores the positional index in the document having tuple item 1 document id.
+  */
+  var inverted = Map[String, ArrayBuffer[(Int, ArrayBuffer[Int])]]().withDefaultValue(ArrayBuffer[(Int, ArrayBuffer[Int])]())
+  var docID = Map[Int, String]()
+  var document_count = 0
+
+  def build_from_string(docs: String): Unit = {
+    for (lines:String <- docs.split("\\r?\\n")){
+      add(lines)
+    }
+  }
+
+  def add(lines: String): Unit ={
+    val line = lines.split("[ ]+")
+    document_count += 1
+    docID = docID + (document_count -> line(0))
+    for (i <- 1 until line.length){
+      val token = line(i)
+      if (!inverted.contains(token)) inverted.put(token, ArrayBuffer[(Int, ArrayBuffer[Int])]((document_count, ArrayBuffer[Int]())))
+      if(inverted(token).last._1 != document_count) inverted(token).append((document_count, ArrayBuffer[Int]()))
+      inverted(token).last._2.append(i)
+    }
+  }
+
+  def search(input: String):ArrayBuffer[(String, Int, Int)] = {
+
+    def closeEnough(first: Int, second: Int, k: Int): Boolean = {
+      return math.abs(first-second) <= k
+    }
+
+    def proximity_intersection(term1: String, term2: String, k: Int): ArrayBuffer[(String, Int, Int)] ={
+      val result = ArrayBuffer[(String, Int, Int)]()
+      var i,j = 0
+      val p1 = inverted(term1)
+      val p2 = inverted(term2)
+
+      //loop through all matching documents
+      while (i < p1.length && j < p2.length){
+        if(p1(i)._1 == p2(j)._1){
+          //check proximity
+          var l = new ArrayBuffer[Int]()
+          var pp1, pp2 = 0
+          while (pp1 < p1(i)._2.length){
+            while(pp2 < p2(j)._2.length){
+              if (closeEnough(p1(i)._2(pp1), p2(j)._2(pp2), k)){
+                l.append(p2(j)._2(pp2))
+              } else if (p2(j)._2(pp2) > p1(i)._2(pp1)){
+                break
+              }
+              pp2 += 1
+            }
+
+            while(l.length != 0 && math.abs(l(0)-p1(i)._2(pp1)) > k){
+              l.remove(0)
+            }
+
+            for (ps <- l){
+              //actually build our result set
+              result.append((docID(p1(i)._1), p1(i)._2(pp1), ps))
+            }
+            pp1 +=1
+          }
+
+          i+=1
+          j+=1
+        } else if (p1(i)._1 < p2(j)._1){
+          i+=1
+        } else {
+          j+=1
+        }
+      }
+      return result
+    }
+
+    val query = input.split("[ ]+")
+    val tolerance = query(1).stripPrefix("/").toInt
+    return proximity_intersection(query(0), query(2), tolerance)
+  }
+}
--- src/test/scala/search/ProximitySpec.scala
+++ src/test/scala/search/ProximitySpec.scala
@@ -2,17 +2,30 @@ package search
 
 import org.scalatest._
 
-class ProximitySpec extends FlatSpec with Matchers {
-  val input = "Doc1 breakthrough  drug  for schizophrenia\nDoc2 new approach for treatment of schizophrenia\nDoc3 new hopes for schizophrenia patients\nDoc4 new schizophrenia drug"
-  val test1 = "hello world"
-  //val index = new booleanIndex()
-  //index.build_from_string(input)
+import scala.collection.mutable.ArrayBuffer
 
-  "A Proximity Search Index" should "return the simple intersection for AND query 1" in {
-    "hello world" should equal (test1)
+class ProximitySpec extends FlatSpec with Matchers {
+  val input = "Doc1 breakthrough drug for schizophrenia\nDoc2 new schizophrenia drug\nDoc3 new drug for treatment of schizophrenia\nDoc4 new hopes for schizophrenia patients"
+  val test1 = "hello world"
+  val index = new PositionalIndex()
+  index.build_from_string(input)
+
+
+
+  "A Proximity Search Index" should "return the proximity intersection for query 1" in {
+      val result = ArrayBuffer[(String, Int, Int)](
+        ("Doc1",4, 2),
+        ("Doc2",2, 3))
+
+     index.search("schizophrenia /2 drug") should be (result)
   }
 
-  it should "return the simple intersection for AND query 2" in {
-    input should equal (input)
+  "A Proximity Search Index" should "return the proximity intersection for query 2" in {
+    val result = ArrayBuffer[(String, Int, Int)](
+      ("Doc1",4, 2),
+      ("Doc2",2, 3),
+      ("Doc3",6, 2))
+
+    index.search("schizophrenia /4 drug") should be (result)
   }
 }
